a25a4454295c4abfd29a736044508c08f26df16a
rakshyak-98 <rakshak1998@gmail.com>
2023-06-29 13:10:14 +0530

    using http module

--- express-assignment/q1.js
+++ express-assignment/q1.js
@@ -1,23 +1,18 @@
-import express from 'express'
-import {productsForMen, productsForWomen} from './data.js'
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-
-app.get("/",  (req, res) => {
-    res.status(200).send("Welcome to Men & Women Dummy Data")
-})
-
-app.get("/men", (req, res) => {
-    res.status(200).json(productsForMen)
-})
-
-app.get("/women", (req, res) => {
-    res.status(200).json(productsForWomen)
-})
-
-app.get("/other", (req, res) => {
-    res.status(404).send("<h1>Page not found! </h1>")
-})
-
-app.listen(3001, () => console.log("express server listening on port 3001"))
+import http from "http";
+import { productsForMen, productsForWomen } from "./data.js";
+const HOST_NAME = "localhost";
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-type", "application/json");
+  if (req.url == "/") res.end("Welcome to Men & Women Dummy Data");
+  if (req.url == "/men") res.end(JSON.stringify(productsForMen));
+  if (req.url == "/women") res.end(JSON.stringify(productsForWomen));
+  if (req.url == "/other") {
+    res.statusCode = 404;
+    res.setHeader("Content-type", "text/html");
+    res.end("<h1>page not found </h1>");
+  }
+});
+server.listen(3001, HOST_NAME, () =>
+  console.log("express server listening on port 3001")
+);
